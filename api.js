4de06a13b88638c307ad868fbdf2d158f3147288
antoineskt <billet.antoine42@gmail.com>
2023-05-12 18:30:04 +0200

    add API http and btn and myList

--- api.js
+++ api.js
@@ -1,3 +1,5 @@
+
+
 document.addEventListener('DOMContentLoaded', function() {
 
 
@@ -8,41 +10,79 @@ document.addEventListener('DOMContentLoaded', function() {
       if (event.key === "Enter") {
         document.querySelector(".sectionBook").innerHTML = "";
         const income = document.querySelector("input").value;
+
+        if (income.length === 0) {
+          alert("write something")
+        } else {
       
-        const response = await fetch("http://openlibrary.org/search.json?title=" + income);
-        
-        const books = await response.json();
-        console.log("CHARGE");
-        console.log(books);
-        
-        for (let i = 0; i < 5 ; i++) {
-
-          const book = books.docs[i];
-          console.log(book)
-
-          //récupération de l'élement dom qui accueillera les articles
-          const sectionBook = document.querySelector(".sectionBook");
-
-          //creation d'une balise/d'un article pour chaque éléments
-          const article = document.createElement("article");
-
-          const titreLivre = document.createElement("h2");
-          titreLivre.innerText = book.title;
+          const response = await fetch("http://openlibrary.org/search.json?title=" + income);
           
-          const imgLivre = document.createElement("img");
-          imgLivre.src = (`https://covers.openlibrary.org/b/id/${book.cover_i}-L.jpg`)
+          const books = await response.json();
+          console.log("CHARGE");
+          console.log(books);
           
-          const authorLivre = document.createElement("p");
-          authorLivre.innerText = (`Author : ${book.author_name[0]}`); 
+          
 
-          const publishDate = document.createElement("p");
-          publishDate.innerText = (`Publish date : ${book.publish_date[0]}`);
-      
-          sectionBook.appendChild(article);
-          article.appendChild(titreLivre);
-          article.appendChild(imgLivre);
-          article.appendChild(authorLivre);
-          article.appendChild(publishDate);
+
+
+          for (let i = 0; i < 5 ; i++) {
+
+            const book = books.docs[i];
+            console.log("je log chaque livre(i) : " + book);
+
+            //récupération de l'élement dom qui accueillera les articles
+            const sectionBook = document.querySelector(".sectionBook");
+
+            //creation d'une balise/d'un article pour chaque éléments
+            const article = document.createElement("article");
+
+            const titreLivre = document.createElement("h2");
+            titreLivre.innerText = book.title;
+            
+            const imgLivre = document.createElement("img");
+            imgLivre.src = (`https://covers.openlibrary.org/b/id/${book.cover_i}-L.jpg`) ?? "no coverage";
+            
+            const authorLivre = document.createElement("p");
+            authorLivre.innerText = (`Author : ${book.author_name[0]}`) ?? "No known author";
+
+            const publishDate = document.createElement("p");
+            publishDate.innerText = (`Publish date : ${book.publish_date[0]}`);
+
+            const buttonReadList = document.createElement("button");
+            buttonReadList.innerText = "Add to my ReadList";
+            buttonReadList.classList.add("buttonReadList");
+            buttonReadList.addEventListener("click", function(){
+              console.log("ajout du livre numéro : " + [i]);
+
+              const books = {
+                id: book.key,
+                title: book.title, 
+                author_name: book.author_name[0],
+                publish_date: book.publish_date[0],
+                cover_i: parseInt(book.cover_i)
+              };
+
+              // Création de la charge utile au format JSON
+              const chargeUtile = JSON.stringify(books);
+              console.log(chargeUtile);
+              //appel de la fonction fetch
+              fetch("http://localhost:8081/books", {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: chargeUtile
+              })
+            });
+        
+            sectionBook.appendChild(article);
+            article.appendChild(titreLivre);
+            article.appendChild(imgLivre);
+            article.appendChild(authorLivre);
+            article.appendChild(publishDate);
+            article.appendChild(buttonReadList);
+
+            
+          };
+          // addToReadList();
         }
       };
   });
---
+++ myList.js
@@ -0,0 +1,44 @@
+document.addEventListener("DOMContentLoaded", async function() {
+
+    const response = await fetch("http://localhost:8081/books");
+    const books = await response.json();
+    console.log("ok")
+    console.log(books);
+    function genererListBook (books) {
+
+        for (let i = 0; i < books.length ; i++ ) {
+            const book = books[i]
+
+            //récupération de l'élement dom qui accueillera les articles
+            const sectionBook = document.querySelector(".sectionBook");
+
+            //creation d'une balise/d'un article pour chaque éléments
+            const article = document.createElement("article");
+
+            const titreLivre = document.createElement("h2");
+            titreLivre.innerText = book.title;
+            
+            const imgLivre = document.createElement("img");
+            imgLivre.src = (`https://covers.openlibrary.org/b/id/${book.cover_i}-L.jpg`) ?? "no coverage";
+            
+            const authorLivre = document.createElement("p");
+            authorLivre.innerText = (`Author : ${book.author_name}`) ?? "No known author";
+
+            const publishDate = document.createElement("p");
+            publishDate.innerText = (`Publish date : ${book.publish_date}`);
+
+
+            sectionBook.appendChild(article);
+            article.appendChild(titreLivre);
+            article.appendChild(imgLivre);
+            article.appendChild(authorLivre);
+            article.appendChild(publishDate);
+            
+
+        }
+
+    }
+    genererListBook(books);
+
+
+});
