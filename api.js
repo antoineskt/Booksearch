aa53884abd54871719fe0993e717bc9bb4777f2e
antoineskt <billet.antoine42@gmail.com>
2023-09-16 14:44:31 +0200

    delete loader img and css improvement

--- api.js
+++ api.js
@@ -16,42 +16,28 @@ document.addEventListener("DOMContentLoaded", function () {
         try {
           //lance la requete de recherche par rapport au nom entrée par l'utilisateur
           const response = await fetch(
-            "http://openlibrary.org/search.json?title=" + income + "&limit=3",
+            "http://openlibrary.org/search.json?title=" +
+              income +
+              "&limit=10" +
+              "&offset=0",
           );
 
           const books = await response.json();
           console.log("CHARGE");
           console.log(books);
-
-          // Suivi du chargement des images
-          let loadedImagesCount = 0;
-          let totalImages = 0;
+          loader.style.display = "none";
 
           for (let i = 0; i < 5; i++) {
             const book = books.docs[i];
-            console.log("je log chaque livre(i) : " + book);
 
             //selection des livres avec couverture
             if (book.cover_i === undefined) {
               console.log("pas de couverture");
             } else {
-              totalImages++; // Incrémente le compteur total d'images à charger
-
               const imgLivre = document.createElement("img");
               imgLivre.src =
                 `https://covers.openlibrary.org/b/id/${book.cover_i}-L.jpg` ??
                 "no coverage";
-
-              // Gestionnaire d'événements de chargement pour chaque image
-              imgLivre.addEventListener("load", function () {
-                loadedImagesCount++;
-
-                // Vérifie si toutes les images sont chargées
-                if (loadedImagesCount === totalImages) {
-                  // Masque le loader une fois que toutes les images sont chargées
-                  loader.style.display = "none";
-                }
-              });
 
               //récupération de l'élement dom qui accueillera les articles
               const sectionBook = document.querySelector(".sectionBook");
@@ -59,8 +45,8 @@ document.addEventListener("DOMContentLoaded", function () {
               //creation d'une balise/d'un article pour chaque éléments
               const article = document.createElement("article");
 
-              const titreLivre = document.createElement("h2");
-              titreLivre.innerText = book.title;
+              //const titreLivre = document.createElement("h2");
+              //titreLivre.innerText = book.title;
 
               const authorLivre = document.createElement("p");
               authorLivre.innerText =
@@ -76,12 +62,14 @@ document.addEventListener("DOMContentLoaded", function () {
               const buttonReadList = document.createElement("button");
               buttonReadList.innerText = "Add to my ReadList";
               buttonReadList.classList.add("buttonReadList");
+
+              //ajout du livre en mémoire
               buttonReadList.addEventListener("click", function () {
                 console.log("ajout du livre numéro : " + [i]);
 
                 const books = {
                   id: parseInt(book.cover_i),
-                  title: book.title,
+                  //title: book.title,
                   author_name: book.author_name[0],
                   publish_date: book.publish_date[0],
                   cover_i: parseInt(book.cover_i),
@@ -99,7 +87,7 @@ document.addEventListener("DOMContentLoaded", function () {
               });
 
               sectionBook.appendChild(article);
-              article.appendChild(titreLivre);
+              //article.appendChild(titreLivre);
               article.appendChild(imgLivre);
               article.appendChild(authorLivre);
               article.appendChild(publishDate);
